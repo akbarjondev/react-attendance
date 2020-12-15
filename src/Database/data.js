57a4855e26d4bf13341cc8225c26b27c2bc19d56
akbarjondev <tojiyev.akbarjon@gmail.com>
2020-12-15 12:35:38 +0500

    Add initial files

---
+++ src/Database/data.js
@@ -0,0 +1,14 @@
+const users = [
+  { id: 1, username: 'Farruh' },
+  { id: 2, username: 'Bekzod' },
+  { id: 3, username: 'Abdullatifxon' },
+  { id: 4, username: 'Umida' },
+  { id: 5, username: 'Iroda' },
+];
+
+const attendance = [
+  { id: 1, userId: 1, visitDate: '2020-12-15', visit: true },
+  { id: 2, userId: 1, visitDate: '2020-12-14', visit: false },
+  { id: 3, userId: 2, visitDate: '2020-12-15', visit: false },
+  { id: 4, userId: 2, visitDate: '2020-12-14', visit: true },
+];
