bc24a3ed61418bb6357cca8ed7dbf6d0532f44a2
akbarjondev <tojiyev.akbarjon@gmail.com>
2020-12-15 14:51:49 +0500

    Add html structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
+  // document.getElementById('date_input').valueAsDate = new Date();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+      <h1 className="h4 bg-primary mt-3 rounded p-2 text-white text-center">O'quvchilar yo'qlamasi</h1>
+
+      <div className="d-flex mt-4 justify-content-center flex-column align-items-center">
+        <p className='text-muted'>Kerakli vaqtni tanlang</p>
+        <input className="form-control w-25" type="date" />
+      </div>
+
+      <table className='table table-bordered mt-3 table-striped'>
+        <thead className='font-weight-bold bg-dark text-white'>
+          <tr>
+            <td>ID</td>
+            <td>Name</td>
+            <td>Visit Date</td>
+            <td>Score</td>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Abbos</td>
+            <td>2020-12-12</td>
+            <td>55</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Valijon</td>
+            <td>2020-12-12</td>
+            <td>25</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Nozima</td>
+            <td>2020-12-12</td>
+            <td>33</td>
+          </tr>
+        </tbody>
+      </table>
+    </div> // End of container
   );
 }
 
---
+++ src/Database/data.jsx
@@ -0,0 +1,21 @@
+const users = [
+  { id: 1, username: 'Farruh' },
+  { id: 2, username: 'Bekzod' },
+  { id: 3, username: 'Abdullatifxon' },
+  { id: 4, username: 'Umida' },
+  { id: 5, username: 'Iroda' },
+];
+
+const attendance = [
+  { id: 1, userId: 1, visitDate: '2020-12-15', visit: true, score: 8 },
+  { id: 2, userId: 1, visitDate: '2020-12-14', visit: false, score: 7 },
+  { id: 3, userId: 2, visitDate: '2020-12-15', visit: false, score: 6 },
+  { id: 4, userId: 2, visitDate: '2020-12-14', visit: true, score: 11 },
+  { id: 5, userId: 3, visitDate: '2020-12-15', visit: true, score: 15 },
+  { id: 6, userId: 3, visitDate: '2020-12-14', visit: true, score: 2 },
+  { id: 7, userId: 4, visitDate: '2020-12-14', visit: true, score: 4 },
+  { id: 8, userId: 4, visitDate: '2020-12-14', visit: false, score: 7 },
+  { id: 9, userId: 4, visitDate: '2020-12-14', visit: true, score: 12 },
+];
+
+export { users, attendance };
